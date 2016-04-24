10a25d7c250ad81cec2070d11d96708165978e82
Henry Morgan <hexmodr@gmail.com>
2016-04-24 18:42:39 +0100

    HI

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,19 @@
 module ApplicationHelper
 
   def render_address address_array
-    render 'address_display', address: address_array
+    render_partial 'address_display', address: address_array
   end
+
+
+  def scope scope_names
+    content_tag :div, id: 'scope_container' do
+      render_scope_contents scope_names
+    end
+  end
+
+  def render_scope_contents scope_names
+
+  end
+
 
 end
